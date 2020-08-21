8c511f3380b87b87c02fa0325c7ceb01f876b15e
priyapsakthivel <priyapsakthivel@gmail.com>
2020-08-21 23:02:49 +0530

    calci

---
+++ constants/calculator.py
@@ -0,0 +1,19 @@
+num1= input("please enter num1")
+num2= input("please enter num2")
+def add(num1,num2):
+    additon=int(num1)+int(num2)
+    print(additon)
+add(num1,num2)
+def sub(num1,num2):
+    subtractiton=int(num1)-int(num2)
+    print(subtractiton)
+sub(num1,num2)
+def multi(num1,num2):
+    multiply=int(num1)*int(num2)
+    print(multiply)
+multi(num1,num2)
+def div(num1,num2):
+    division=int(num1)/int(num2)
+    print(division)
+div(num1,num2)
+
