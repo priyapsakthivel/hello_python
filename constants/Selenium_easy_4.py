196519adbf74bcba27dd1a65db6dd3ec8bbc619a
priyapsakthivel <priyapsakthivel@gmail.com>
2020-12-15 22:55:13 +0530

    selenium easy2

--- constants/Selenium_easy_4.py
+++ constants/Selenium_easy_4.py
@@ -5,8 +5,6 @@ def dropdown():
     driver.get("https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html")
     driver.maximize_window()
     driver.execute_script("scrollTo(0,200)")
-    drop=driver.find_element_by_id("select-demo")
-    drop.click()
-    drop.text
-    drop.select_by_index(0)
+    drop=driver.find_element_by_id("select-demo").text
+    drop.select_by_visible_text('Sunday')
 dropdown()
