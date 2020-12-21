35f0ad19259625836972a5dc1b085321cb5b221e
priyapsakthivel <priyapsakthivel@gmail.com>
2020-12-21 22:37:41 +0530

    Flipkart_login.py

--- constants/Flipkart_login.py
+++ constants/Flipkart_login.py
@@ -3,10 +3,11 @@ import time
 from selenium import webdriver
 driver=webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
 
+mobile_number= input("pleae enter your phone number")
 def flipkart_login(driver):
     driver.get("https://www.flipkart.com/")
     driver.maximize_window()
-    driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input").send_keys("9042003857")
+    driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input").send_keys(mobile_number)
     driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[4]/button").click()
 flipkart_login(driver)
 time.sleep(2)
