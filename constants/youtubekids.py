6ab797cf08ddb4f62f40c39cf38afe431803a850
priyapsakthivel <priyapsakthivel@gmail.com>
2021-01-04 22:28:55 +0530

    kidsapp

---
+++ constants/youtubekids.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+
+driver=webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
+
+def launch(driver):
+    driver.get("https://www.youtubekids.com/")
+    driver.maximize_window()
+    driver.find_element_by_id()
+launch(driver)
+time.sleep(5)
+
+def choose_kids(driver):
+    driver.find_element_by_id("kid-button").click()
+    time.sleep(2)
+    driver.find_element_by_id("ok-button").click()
+    time.sleep(2)
+choose_kids(driver)
