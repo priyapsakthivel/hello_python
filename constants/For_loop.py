6059d59d031ae71ebe3d524978782c972470eef7
priyapsakthivel <priyapsakthivel@gmail.com>
2021-01-07 23:06:49 +0530

    kidsapps

--- constants/For_loop.py
+++ constants/For_loop.py
@@ -1,4 +1,8 @@
-for x in range(5):
-    print(x)
-    for y in range(5):
-        print(y)
+# for x in range(5):
+#     print(x)
+#     for y in range(5):
+#         print(y)
+
+car=["car1","car2","car3","car4"]
+for x in car():
+    print(car(x))
