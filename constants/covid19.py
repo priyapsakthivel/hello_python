4647b3462062d091b4839fe2084ffcf89d02ea57
priyapsakthivel <priyapsakthivel@gmail.com>
2021-01-17 11:01:54 +0530

    kidsapp

--- constants/covid19.py
+++ constants/covid19.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
 
 driver=webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
 def launch(driver):
@@ -10,10 +8,6 @@ launch(driver)
 
 def search(driver):
     search_box=input("Search your district or state")
-    search_input= driver.find_element_by_xpath("//*[@id='root']/div/div[3]/div[1]/div[1]/div[1]/div[2]")
+    search_input= driver.find_element_by_xpath("/html/body/div/div/div[3]/div[1]/div[1]/div[1]/div[2]")
     search_input.send_keys(search_box)
-    search_input.send_keys(Keys.ENTER)
-    driver.find_element_by_xpath("//*[@id='root']/div/div[3]").click()   #to stop extra suggetion
-
-
 search(driver)
