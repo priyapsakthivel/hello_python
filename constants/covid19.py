e13c88331b091938ca95aa9843c8895f5628a276
priyapsakthivel <priyapsakthivel@gmail.com>
2021-01-21 22:05:01 +0530

    kidsapp

--- constants/covid19.py
+++ constants/covid19.py
@@ -12,9 +12,9 @@ def search(driver):
     search_box=input("Search your district or state")
     search_input= driver.find_element_by_xpath("/html/body/div/div/div[3]/div[1]/div[1]/div[1]/div[2]/input")
     search_input.send_keys(search_box+" ")
-    time.sleep(2)
+    time.sleep(1)
     driver.find_element_by_xpath("/html/body/div/div/div[3]/div[1]/div[1]/div[1]/div[3]/a/div").click()
-    time.sleep(2)
+    time.sleep(1)
     confirmed = driver.find_element_by_xpath("/html/body/div/div/div[3]/div[1]/div[2]/div[1]/div[2]").text
     print("confirmed cases :" + confirmed)
     time.sleep(2)
