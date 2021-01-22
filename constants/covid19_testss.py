6a1670ee4ca3a47853d732a55711c4b8f864467a
priyapsakthivel <priyapsakthivel@gmail.com>
2021-01-22 23:12:26 +0530

    covid19_testss.py

--- constants/covid19_testss.py
+++ constants/covid19_testss.py
@@ -1,2 +1,7 @@
 from selenium import webdriver
 driver=webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
+
+def launch(driver):
+    driver.get("https://www.covid19india.org/")
+    driver.maximize_window()
+launch(driver)
