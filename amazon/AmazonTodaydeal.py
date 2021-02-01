b06221c4f4733a4055e88bd5465a294b3aa438da
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-01 22:27:07 +0530

    estopwatch

--- amazon/AmazonTodaydeal.py
+++ amazon/AmazonTodaydeal.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 
 driver=webdriver.Edge(executable_path="P:\Webdrivers\edgedriver.exe")
 driver.get("https://www.amazon.in/")
-time.sleep(5)
+time.sleep(2)
 driver.find_element_by_id("nav-link-accountList").click()
 driver.maximize_window()
 driver.find_element_by_xpath("/html/body/div[1]/header/div/div[2]/div[3]/div[2]/div/a[3]").click()
