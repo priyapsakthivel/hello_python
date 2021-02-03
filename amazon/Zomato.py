b67c186144f6c5d12c0122f4afea1fad51faf544
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-03 22:02:16 +0530

    estopwatch

--- amazon/Zomato.py
+++ amazon/Zomato.py
@@ -7,5 +7,5 @@ def launch(driver):
     driver.get("https://www.zomato.com/akola/restaurants?context=delivery")
     driver.maximize_window()
     driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/header/nav/ul[2]/li[2]/a").send_keys("9080444947")
-    time.sleep(2)
+    time.sleep(5)
 launch(driver)
