42c2bcef2be86b3ecb485dc2dece6661f5bb8331
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-03 22:01:12 +0530

    estopwatch

---
+++ amazon/Zomato.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+
+driver= webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
+def launch(driver):
+    driver.get("https://www.zomato.com/akola/restaurants?context=delivery")
+    driver.maximize_window()
+    driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/header/nav/ul[2]/li[2]/a").send_keys("9080444947")
+    time.sleep(2)
+launch(driver)
