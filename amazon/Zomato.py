5e171fe667816e881fb206afef5b5f7649d1b9d1
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-04 21:38:31 +0530

    zomato

--- amazon/Zomato.py
+++ amazon/Zomato.py
@@ -9,5 +9,5 @@ def launch(driver):
     driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/header/nav/ul[2]/li[2]/a").click()
     time.sleep(2)
     driver.find_element_by_xpath("/html/body/div[6]/div/div[2]/section[2]/section/div[1]/div/input").send_keys("9080444947")
-    time.sleep(3)
+    time.sleep(2)
 launch(driver)
