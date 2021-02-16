5589abb39c687c1cd8a29f6ad207f5fc5ee4168f
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-16 22:03:57 +0530

    K

--- constants/summingup.py
+++ constants/summingup.py
@@ -1,12 +1,12 @@
-
-def sum():
-    sum1=11;
-    sum2=12;
-    sum3=45;
-    sum4=45;
+class sumup():
+    def sum():
+        sum1=11;
+        sum2=12;
+        sum3=45;
+        sum4=45;
 def addingup(sum1,sum2,sum3,sum4):
     print("sum of first 2 numbers"+int(sum1)+int(sum2));
     print("sum of first 2 numbers"+int(sum3)+int(sum4));
     print("sum of first 2 numbers"+int(sum3)+int(sum2));
 sum()
-addingup(sum1,sum2,sum3,sum4)
+addingup()
