3ec2e40c3c92316daa45eaf6063ab91988386385
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-22 20:20:20 +0530

    selenium1

--- python_learn/selenium_1.py
+++ python_learn/selenium_1.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 driver=webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
 
 def launch():
-    driver.get("P:\Webdrivers\msedgedriver")
+    driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
     driver.maximize_window()
 launch()
+def popclose():
+    driver.find_element_by_id("at-cv-lightbox-close").click()
