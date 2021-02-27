a1ddedb133a22fe00472011f953c86f07a779e03
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-27 20:16:49 +0530

    selenium2

--- python_learn/Selenium_2.py
+++ python_learn/Selenium_2.py
@@ -3,8 +3,6 @@ import time
 from selenium import webdriver
 
 driver=webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
-a=input("please enter any number1")
-b=input("please enter any number2")
 
 def launching(driver):
     driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
@@ -15,7 +13,12 @@ def popupclose(driver):
     time.sleep(2)
     driver.execute_script("scrollTo(0,200)")
 popupclose(driver)
+a=input("please enter any number1")
+b=input("please enter any number2")
+
 def twoinputfield(driver,a,b):
     driver.find_element_by_id("sum1").send_keys(a)
     driver.find_element_by_id("sum2").send_keys(b)
     time.sleep(2)
+    driver.find_element_by_xpath("//*[@id='gettotal']/button").click()
+twoinputfield(driver,a,b)
