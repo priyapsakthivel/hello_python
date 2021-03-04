c70dc25a096c6c22488e49e56e27ce082903a691
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-04 22:25:49 +0530

    selenium3

--- python_learn/Selenium+3.py
+++ python_learn/Selenium+3.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
 driver=webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
-def
-driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html")
+def launching(driver):
+    driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html")
+    driver.maximize_window
+launching(driver)
