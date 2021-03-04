c58729c5187cc1355d074788b1c7312aa6bcceea
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-04 22:22:10 +0530

    selenium3

---
+++ python_learn/Selenium+3.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+driver=webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
+def
+driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html")
