6e9ef5e8d6903f8255835bc8e913ce6dcc077f46
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-08 20:11:11 +0530

    selenium4

---
+++ python_learn/Selenium_4.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+driver= webdriver.Edge(executable_path="P:\Webdrivers\msedgedriver")
+def launch():
+    driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html")
+    driver.maximize_window()
+    driver.execute_script("scrollTo(0,200)")
+launch()
