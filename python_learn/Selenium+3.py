69ac50dea2fa9569df7643bcd322ccaf2538caf6
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-08 20:04:16 +0530

    selenium3

--- python_learn/Selenium+3.py
+++ python_learn/Selenium+3.py
@@ -11,7 +11,7 @@ def bootstrap(driver):
     driver.execute_script("scrollTo(0,200)")
     driver.find_element_by_xpath("//*[@id='sandbox-container1']/div/span").click()
     driver.find_element_by_xpath("/html/body/div[3]/div[1]/table/tfoot/tr[1]/th").click()
-    time.sleep(12)
+    time.sleep(10)
     driver.find_element_by_xpath("//*[@id='sandbox-container1']/div/span").click()
     driver.find_element_by_xpath("/html/body/div[3]/div[1]/table/tfoot/tr[2]/th").click()
 bootstrap(driver)
