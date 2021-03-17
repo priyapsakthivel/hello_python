572d9d833a8ff69c3299f7ed43e038e8356761fa
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-17 22:41:32 +0530

    Kselenium4

--- python_learn/Number_swap.py
+++ python_learn/Number_swap.py
@@ -1,6 +1,6 @@
-x=input("please enter a number")
-x=input("please enter a number")
-b=12
-a,b=b,a
+x=input("please enter a number1")
+y=input("please enter a number2")
+def numbers(x,y)
+x,y,b=b,a
 print("a=",a)
 print("b=",b)
