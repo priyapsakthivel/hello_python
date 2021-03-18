8baefe30eb469a59650a78d6cf0fd8ef861528db
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-18 22:30:29 +0530

    Kselenium4

--- python_learn/prime_number.py
+++ python_learn/prime_number.py
@@ -1,5 +1,5 @@
-for i in range(1,11):
-    if(int(i)%int(i)==0) & int(i/1)==int(i):
+for i in range(2,11):
+    if(int(i)/int(i)==0) & int(i/1)==int(i):
         print(i,"its a prime number")
     else:
         print(i,"its a composite number")
