c263c0110f69d1a3a206b52d0119e306163ffe3d
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-18 22:46:51 +0530

    Kselenium4

--- python_learn/prime_number.py
+++ python_learn/prime_number.py
@@ -1,5 +1,14 @@
 for i in range(2,11):
-    if(int(i)/int(i)==0) & int(i/1)==int(i):
+    for j in range
+    if(int(i)%int(i)==0) & int(i%1)==0 & int(i)!/):
         print(i,"its a prime number")
+        if(i%2==0):
+            print(i,"its an even number")
+        else:
+            print("its odd number")
     else:
         print(i,"its a composite number")
+        if (i % 2 == 0):
+            print(i, "its an even number")
+        else:
+            print("its odd number")
