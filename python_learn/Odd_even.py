c915a9c2297f8ef748729af40da5d71b80eb551a
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-18 22:08:38 +0530

    Kselenium4

---
+++ python_learn/Odd_even.py
@@ -0,0 +1,6 @@
+number1=int(input("please enter an number"))
+number2=int(input("please enter a second number"))
+if (number1%2==0):
+    print("its an even number")
+else:
+    print("the given number is odd")
