e9222edab2dfed368f0fe3d51fc113695f5b61e0
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-19 23:38:01 +0530

    Kselenium4

--- python_learn/prime_number.py
+++ python_learn/prime_number.py
@@ -1,7 +1,7 @@
 for i in range(2,11):
     for j in range(1,10):
         condition=int (j)<int(i)&int(i)<int(j)
-    if(int(i)%int(i)==0) & int(i%1)==0 & int(i)%int(condition)==0:
+    if(int(i)%int(i)==1) & int(i%1)==0 & int(i)%int(condition)==0:
         print(i,"its a prime number")
         if(i%2==0):
             print(i,"its an even number")
