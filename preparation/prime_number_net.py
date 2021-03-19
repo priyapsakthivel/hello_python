e6e5977739c6c4fe7164a1c360bb38fcf53ebf0b
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-19 23:56:46 +0530

    Kselenium4

--- preparation/prime_number_net.py
+++ preparation/prime_number_net.py
@@ -2,6 +2,6 @@ number= int(input("please enter a number"))
 if number >1:
     for i in range(2,number):
         if (number%i)==0:
-            print("its a prime number")
-        else:
             print("its a composite number")
+        else:
+            print("its a prime number")
