02468c8d60cf198efc3b4dbdf8e55177a7f6ddd5
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-20 23:06:28 +0530

    Kselenium4

--- python_learn/operations_difference.py
+++ python_learn/operations_difference.py
@@ -7,3 +7,7 @@ print (x)
 
 x-=3
 print(x) #prints 4 as in the previous operation the end result of x has become 7 soo on reducing 3 its 4 and print 4 as result
+
+y= str("i love India")
+for i in y:
+    print(y,end="")
