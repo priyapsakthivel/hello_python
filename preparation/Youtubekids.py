d2adb0f561850c880c3dedbc23af8e95d489dd33
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-21 22:46:45 +0530

    Kselenium4

--- preparation/Youtubekids.py
+++ preparation/Youtubekids.py
@@ -11,6 +11,7 @@ def i_am_parent(driver):
     driver.find_element_by_id("parent-button").click()
     driver.find_element_by_id("next-button").click()
 i_am_parent(driver)
+time.sleep(2)
 def hi_parent(driver):
     driver.find_element_by_id("digits-input").send_keys("1997")
 hi_parent(driver)
