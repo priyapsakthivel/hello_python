35b38b5974c022fc903197c068551cedd8c12a67
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-21 22:55:44 +0530

    Kselenium4

--- preparation/Youtubekids.py
+++ preparation/Youtubekids.py
@@ -11,7 +11,7 @@ def i_am_parent(driver):
     driver.find_element_by_id("parent-button").click()
     driver.find_element_by_id("next-button").click()
 i_am_parent(driver)
-time.sleep(2)
+time.sleep(3)
 def hi_parent(driver):
     driver.find_element_by_xpath("/html/body/ytk-app/div/ytk-kids-onboarding-flow-data-v2/div[3]/ytk-kids-onboarding-age-gate-renderer/div/div[4]").send_keys("1997")
 hi_parent(driver)
