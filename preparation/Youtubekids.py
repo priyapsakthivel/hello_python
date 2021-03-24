91c9cef248aeb86a0f03965b37bc767741c41def
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-24 22:22:18 +0530

    Kselenium4

--- preparation/Youtubekids.py
+++ preparation/Youtubekids.py
@@ -17,10 +17,16 @@ class youtubekids():
     def i_am_parent(driver):
         driver.find_element_by_id("parent-button").click()
         driver.find_element_by_id("next-button").click()
+
     i_am_parent(driver)
     time.sleep(4)
 
-    def hi_parent(driver):
-        driver.find_element_by_id("digits-input").send_keys("1997")
-    hi_parent(driver)
+    def hi_parent(driver, year):
+        for x in range(1, 5):
+            driver.find_element_by_xpath(
+                "/html/body/ytk-app/div/ytk-kids-onboarding-flow-data-v2/div[3]/ytk-kids-onboarding-age-gate-renderer/div/div[4]/input["+str(x)+"]").send_keys(
+                year(x - 1))
 
+    year = input("please enter the year of birth")
+    hi_parent(driver,list(year))
+    driver.find_element_by_id("submit-button").click()
