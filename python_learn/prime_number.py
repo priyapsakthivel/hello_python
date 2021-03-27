b68822ac10413f57e2cc318c4f8d4d9b13c876ad
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-27 20:45:17 +0530

    Kselenium4

--- python_learn/prime_number.py
+++ python_learn/prime_number.py
@@ -3,7 +3,7 @@ for i in range(2,11):
         condition=int (j)<int(i)&int(i)<int(j)
     if(int(i)%int(i)==0) & int(i%1)==0 & int(i)%int(condition)==0:
         print(i,"its a prime number")
-        if(i%2==0):
+        if(i%2==1):
             print(i,"its an even number")
         else:
             print("its odd number")
