525f1e2105fdeca7d429091abf79f3a0a061050a
priyasakthivel <priyapsakthivel@gmail.com>
2021-03-30 16:25:55 +0530

    test

---
+++ flipkart_1/endless_loop_fix.py
@@ -0,0 +1,17 @@
+def hello_world():
+    for i in range(5):
+        value = hello_alien(i)
+        if (value):
+            break
+        else:
+            continue
+
+def hello_alien(n):
+    if n == 3:
+        print("im in True")
+        return True
+    else:
+        print("im in false")
+        return False
+
+hello_world()
