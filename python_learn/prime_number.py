8ec30edea001dbdc80f67a6a19f256e68fd8c302
priyapsakthivel <priyapsakthivel@gmail.com>
2021-04-01 21:57:27 +0530

    Kselenium4

--- python_learn/prime_number.py
+++ python_learn/prime_number.py
@@ -1,4 +1,4 @@
-for i in range(2,11):
+for i in range(1,11):
     for j in range(1,10):
         condition=int (j)<int(i)&int(i)<int(j)
     if(int(i)%int(i)==0) & int(i%1)==0 & int(i)%int(condition)==0:
