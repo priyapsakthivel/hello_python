a5cdaf966121cabba92a5a93ffa8d1691fd23b99
priyapsakthivel <priyapsakthivel@gmail.com>
2021-04-09 22:09:38 +0530

    selenium

--- python_learn/prime_number.py
+++ python_learn/prime_number.py
@@ -1,5 +1,5 @@
 for i in range(1,20):
-    for j in range(1,20):
+    for j in range(1,10):
         condition=int (j)<int(i)&int(i)<int(j)
     if(int(i)%int(i)==0) & int(i%1)==0 & int(i)%int(condition)==0:
         print(i,"its a prime number")
